554c6f85a1168538c879e7783c39a5e664421bfc
rolf999 <rafal_grzelec@vp.pl>
2017-08-20 21:29:15 +0200

    v8
    dodano menu glowne i miejsce na wstawke fabularna

---
+++ Statki/Assets/MainMenu/Scripts/MainMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    public Texture backround;
+
+    public bool story;
+
+    public TextAsset file;
+    
+    void OnStart()
+    {
+        story = false;
+    }
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backround);
+        GUIStyle style = new GUIStyle("TextField");
+        style.fontSize = (int)(Screen.height * 0.1);
+        style.alignment = TextAnchor.MiddleCenter;
+
+
+        if (!story)
+        {
+            
+
+            if (GUI.Button(new Rect(Screen.width * .375f, Screen.height * .45f, Screen.width * .25f, Screen.height * .12f), "New Game", style))
+                story = true;
+        }
+        else
+        {
+            
+
+            GUIStyle labelStyle = new GUIStyle("TextField");
+            labelStyle.fontSize = (int)(Screen.height * 0.04);
+
+            GUI.Label(new Rect(Screen.width * .2f, Screen.height * .2f, Screen.width * .6f, Screen.height * .5f), file.ToString(), labelStyle);
+
+            if (GUI.Button(new Rect(Screen.width * .375f, Screen.height * .8f, Screen.width * .25f, Screen.height * .12f), "Play", style))
+            {
+                Application.LoadLevel("level1");
+            }
+        }
+    }
+}
---
+++ Statki/Assets/Scripts/enemy/BossShoot.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossShoot : MonoBehaviour {
+
+    public Vector3 bulletOffset = new Vector3(0, 0.5f, 0);
+    GameObject toSound;
+
+    public GameObject bulletPrefab;
+    int bulletLayer;
+
+    public float fireDelay = 0.25f;
+    float cooldownTimer = 0;
+
+    public EnemyParam enemyParam;
+
+    void Start()
+    {
+        enemyParam = transform.gameObject.GetComponent<EnemyParam>();
+        bulletLayer = gameObject.layer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyParam.fire)
+        {
+            cooldownTimer -= Time.deltaTime;
+
+            if (cooldownTimer <= 0)
+            {
+                // SHOOT!
+                cooldownTimer = fireDelay;
+
+                Vector3 offset = transform.rotation * bulletOffset;
+
+                offset.y += 20;
+                offset.x += 20;
+                GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, transform.position + offset, transform.rotation);
+                offset.y += 20; 
+                GameObject bulletGO2 = (GameObject)Instantiate(bulletPrefab, transform.position + offset, transform.rotation);
+                AudioSource M4A1 = GetComponent<AudioSource>();
+                M4A1.Play();
+                bulletGO.layer = bulletLayer;
+                bulletGO2.layer = bulletLayer;
+            }
+
+        }
+    }
+}
