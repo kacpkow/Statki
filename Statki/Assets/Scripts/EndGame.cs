21b2d794d77d2fc81c26014ce6934229ac775100
RepubliC <kubow15@hotmail.com>
2017-09-21 07:42:13 +0200

    [wind] delete force from view

--- Statki/Assets/Scripts/EndGame.cs
+++ Statki/Assets/Scripts/EndGame.cs
@@ -5,16 +5,22 @@ using UnityEngine.UI;
 
 public class EndGame : MonoBehaviour {
 
+    public PlayerSpawner player;
+    public GameObject endGameWindow;
+
+    public GameObject boss;
+
     public Button exit;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        boss = GameObject.Find("boss1(Clone)");
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 
     public void exitOnClick()
